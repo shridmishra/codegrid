fb0ef579539fd3a9707f6e83d6b035bb68228935
Shrid Mishra <130430229+shridmishra@users.noreply.github.com>
2026-08-10 11:02:50 +0530

    feat: add templates/HTML/CGMWTJAN2026

---
+++ templates/HTML/CGMWTJAN2026/deadspace/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        work: resolve(__dirname, "lab.html"),
+        culture: resolve(__dirname, "work.html"),
+        directors: resolve(__dirname, "project.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+    assetsInclude: [
+      "**/*.jpeg",
+      "**/*.jpg",
+      "**/*.png",
+      "**/*.svg",
+      "**/*.gif",
+    ],
+    copyPublicDir: true,
+  },
+});

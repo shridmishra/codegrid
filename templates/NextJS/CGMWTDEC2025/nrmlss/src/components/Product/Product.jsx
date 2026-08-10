bfe887d874e96fa8b039557bcf0bd2a675741fc3
Shrid Mishra <130430229+shridmishra@users.noreply.github.com>
2026-08-10 11:08:42 +0530

    feat: add templates/NextJS/CGMWTDEC2025

---
+++ templates/NextJS/CGMWTDEC2025/nrmlss/src/components/Product/Product.jsx
@@ -0,0 +1,48 @@
+"use client";
+import "./Product.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { useCartStore } from "@/store/cartStore";
+
+const Product = ({
+  product,
+  productIndex,
+  showAddToCart = true,
+  className = "",
+  innerRef,
+  style,
+}) => {
+  const addToCart = useCartStore((state) => state.addToCart);
+  const pathname = usePathname();
+
+  const handleImageClick = () => {
+    if (pathname === "/unit") {
+      window.dispatchEvent(new CustomEvent("scrollToTop"));
+    }
+  };
+
+  return (
+    <div className={`product ${className}`} ref={innerRef} style={style}>
+      <Link href="/unit" className="product-img" onClick={handleImageClick}>
+        <img src={`/products/product_${productIndex}.png`} alt={product.name} />
+      </Link>
+      <div className="product-info">
+        <div className="product-info-wrapper">
+          <p>{product.name}</p>
+          <p>${product.price}</p>
+        </div>
+        {showAddToCart && (
+          <button
+            className="add-to-cart-btn"
+            onClick={() => addToCart(product)}
+          >
+            Add to Cart
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
